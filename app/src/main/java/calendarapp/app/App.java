910bc8edbd2378edd18eb32aef5e3edbf558921f
franzis17 <franzlabrador994@yahoo.com.au>
2023-10-29 22:13:59 +0800

    a

--- app/src/main/java/calendarapp/app/App.java
+++ app/src/main/java/calendarapp/app/App.java
@@ -7,7 +7,7 @@ public class App
     public static void main(String[] args)
     {
         runCalendarApp(args);
-        //test();
+        //test();a
     }
     
     public static void runCalendarApp(String[] args)
